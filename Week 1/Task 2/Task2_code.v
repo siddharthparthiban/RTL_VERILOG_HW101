fdb686b154c0343f0d209fd53be0c97b679eea3b
P SIDDHARTH <145111693+siddharthparthiban@users.noreply.github.com>
2025-02-23 10:03:41 +0530

    Update Task2_code.v

--- Week 1/Task 2/Task2_code.v
+++ Week 1/Task 2/Task2_code.v
@@ -1,1 +1,24 @@
+#AlwaysBlock1
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    assign out_assign=a&b;
+    always@(*) out_alwaysblock=a&b;
+endmodule
 
+
+#AlwaysBlock2
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+    assign out_assign=a^b;
+    always@(*) out_always_comb=a^b;
+    always@(posedge clk) out_always_ff=a^b;
+endmodule
